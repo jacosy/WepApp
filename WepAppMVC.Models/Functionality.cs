4c0c96f0762fa06239113b69b77c7b6eb661eec3
Longoria You <longoria.you@gmail.com>
2019-04-07 12:21:21 +0800

    Create Functionality Related Data

---
+++ WepAppMVC.Models/Functionality.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace WepAppMVC.Models
+{
+    public class Functionality
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Decoration { get; set; }
+        public int ParentId { get; set; }
+        public int Priority { get; set; }
+        public DateTime CreateTime { get; set; }
+        public string CreateUser { get; set; }
+    }
+}
---
+++ WepAppMVC.Models/RoleFunctionMappings.cs
@@ -0,0 +1,9 @@
+﻿namespace WepAppMVC.Models
+{
+    public class RoleFunctionMappings
+    {
+        public int Id { get; set; }
+        public string RoleId { get; set; }
+        public int FunctionalityId { get; set; }
+    }
+}
